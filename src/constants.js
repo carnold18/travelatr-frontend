446557868c3b515e33708f6b183e14d282f59b93
carnold18 <chrisarnold2010@gmail.com>
2019-12-30 03:49:02 -0800

    testing

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,4 @@
 
 // This is the api url when running a local server. 
 // Be sure to update the url below if the backend is deployed at any point.
-// Test
 export const API_URL = "http://localhost:3000/";
